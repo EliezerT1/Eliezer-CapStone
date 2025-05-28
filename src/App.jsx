b7ca4d1a6bf4dafac0824cce21c9fb3f5696d586
eliezert1 <eliezer.then@accenture.com>
2025-05-28 15:28:24 -0400

    first

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './components/Header/Header';
+import Body from './components/Body/Body';
+import './App.css';
+
+function App() {
+
+  return (
+      <div className='overflow-hidden w-full'>
+        <Header />
+        <Body />
+ 
+      </div>
+  )
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='w-full'>
+            <nav className='bg-white/50 text-black p-4 shadow-lg'>
+                <div className='flex items-center justify-between'>
+                    <a href="#" className="text-2xl font-bold">Hi Chat</a>
+                </div>
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
